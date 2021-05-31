956f77611707f3810c033f7aa19f24d9f889750b
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-31 12:52:06 +0530

    Added UC4 : solve using switch case

--- empWage/src/empWage/WageCalculator.java
+++ empWage/src/empWage/WageCalculator.java
@@ -17,17 +17,35 @@ public class WageCalculator {
     }
     public static void calcDailyWage() {
     	int dailyWage = wagePerHr*hrPerDay;
-    	System.out.println("Dailt wage of full time employee is "+dailyWage);
+    	System.out.println("Daily wage of full time employee is "+dailyWage);
     }
     public static void partTimeEmp(){
     	int partDailyWage=wagePerHr*partTimeHr;
     	System.out.println("Daitly Wage of part time employee is "+partDailyWage);
+    }
+    public static void Switch() {
+    	int exp;
+    	Random random=new Random();
+    	exp=1+random.nextInt(2);
+    	switch(exp) {
+    	case 1:
+    		System.out.println("Employee is fulltime");
+    		calcDailyWage();
+    		break;
+    	case 2:
+    		System.out.println("Employee is parttime");
+    		partTimeEmp();
+    		break;
+    	default:
+    		System.out.println("Invalid");
+    	}
+    	
     }
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculator");
 		attendence();
 		calcDailyWage();
 		partTimeEmp();
-		
+		Switch();
 	}
 }
