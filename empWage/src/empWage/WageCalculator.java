823d7a2609c20491de14812162f1a99c88955f5c
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-31 12:04:47 +0530

    Added UC1 i.e. attendence()

--- empWage/src/empWage/WageCalculator.java
+++ empWage/src/empWage/WageCalculator.java
@@ -1,8 +1,23 @@
 package empWage;
-
+import java.util.Random;
 public class WageCalculator {
-
+    
+    public static void attendence() {
+    	int ran;
+    	int isPresent=1;
+    	Random random=new Random();
+    	ran=random.nextInt(2);
+		System.out.println(ran);
+		if(ran==isPresent)
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee absent");
+		
+    }
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculator");
+		//Random random=new Random();
+		attendence();	
+		
 	}
 }
